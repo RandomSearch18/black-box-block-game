a71a518ca7bee32a149204add5518c8563d23403
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-03-11 00:56:23 +0000

    improve MRE

--- pixels-drawn-wrong-row.c
+++ pixels-drawn-wrong-row.c
@@ -1,11 +1,11 @@
 #include "blackbox.h"
 
 int pixels[8] = {0};
-int sprite_x = 4;
-int sprite_y = 0;
+// Coords of the pixel
+int x = 4;
+int y = 1;
 
 void setup() {
-  pixels[sprite_y + 1] |= 1 << (7 - sprite_x);
-  debug_print("%d: %d", sprite_y + 1, pixels[sprite_y + 1]);
+  pixels[y] |= 1 << (7 - x);
   bb_matrix_set_arr(pixels);
 }
